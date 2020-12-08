31bc6f84b63c98cb13b0eb9abe0554937b3e1aaa
slagroom <sl4gr00m@gmail.com>
2020-12-07 22:47:04 -0400

    day 2 python

---
+++ 2020/02/python/main.py
@@ -0,0 +1,29 @@
+import re
+import sys
+
+validPart1 = 0
+validPart2 = 0
+pattern = '^(\d+)-(\d+)\s+(.):\s+(.*)$'
+for line in map(lambda l: l.rstrip(), sys.stdin):
+
+    match = re.search(pattern, line)
+    first = int(match.group(1))
+    second = int(match.group(2))
+    char = match.group(3)[0]
+    password = match.group(4)
+
+    matching_chars = len([c for c in  match.group(4) if c == char])
+
+    if first <= matching_chars and matching_chars <= second:
+        validPart1 += 1
+
+    if len(password) > (first-1) and password[first-1] == char:
+        if len(password) <= (second-1) or password[second-1] != char:
+            validPart2 += 1
+    else:
+        if len(password) > (second-1) and password[second-1] == char:
+            validPart2 += 1
+
+
+print(f'part 1: {validPart1}')
+print(f'part 2: {validPart2}')
